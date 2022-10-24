0051e35a4fd19d2b5465bd0dfe067b27601364f6
John Parks <john.parks@knowledgelake.com>
2022-10-24 10:51:25 -0500

    Changed Site Colors + Created JS Form FUnctions

--- frontend/javascript/apps.js
+++ frontend/javascript/apps.js
@@ -14,58 +14,78 @@ document
     }
   });
 
-//Working on populating table from form
+let selectedRow = null;
 
-document
-  .querySelector(".add-stop-button")
-  .addEventListener("click", function () {
-    const name = document.querySelector(".name").value;
-    const street = document.querySelector(".input-street").value;
-    const city = document.querySelector(".input-city").value;
-    const state = document.querySelector(".input-state").value;
-    const zip = document.querySelector(".input-zip").value;
-    const phone = document.querySelector(".phone").value;
-    const key = document.querySelector(".key").value;
-    const quantity = document.querySelector(".quantity").value;
-  });
+function onFormSubmit(e) {
+  event.preventDefault();
+  let formData = readFormData();
+  if (selectedRow === null) {
+    insertNewRecord(formData);
+  } else {
+    updateRecord(formData);
+  }
+  resetForm();
+}
 
-let list1 = [];
-let list2 = [];
-let list3 = [];
-let list4 = [];
-let list5 = [];
-let list6 = [];
+//Retrieving form data
+function readFormData() {
+  var formData = {};
+  formData["c-name"] = document.getElementById("c-name").value;
+  formData["input-street"] = document.getElementById("input-street").value;
+  formData["input-city"] = document.getElementById("input-city").value;
+  formData["input-state"] = document.getElementById("input-state").value;
+  formData["input-zip"] = document.getElementById("input-zip").value;
+  formData["phone"] = document.getElementById("phone").value;
+  formData["key"] = document.getElementById("key").value;
+  formData["quantity"] = document.getElementById("quantity").value;
+  formData["stop-notes"] = document.getElementById("stop-notes").value;
 
-let n = 1;
-let x = 0;
+  return formData;
+}
 
-function addTableRow() {
-  let addRow = document.getElementById("table-results");
-  let newRow = addRow.insertRow(n);
+//Insert the data
+function insertNewRecord(data) {
+  var table = document
+    .getElementsByClassName(".table-results")
+    .getElementsByTagName("tbody")[0];
+  var newRow = table.insertRow(table.length);
+  var cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.c - name;
+  var cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.input - street;
+  var cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.phone;
+  var cell4 = newRow.insertCell(3);
+  cell4.innerHTML = data.key;
+  var cell5 = newRow.insertCell(4);
+  cell4.innerHTML = data.quantity;
+  var cell6 = newRow.insertCell(6);
+  // cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`;
+}
 
-  list1[x] = n++;
-  list2[x] = document.getElementById("name").value;
-  list3[x] = document.getElementById("input-street").value;
-  list4[x] = document.getElementById("phone").value;
-  list5[x] = document.getElementById("key").value;
-  list6[x] = document.getElementById("quantity").value;
+//Edit the data
+function onEdit(td) {
+  selectedRow = td.parentElement.parentElement;
+  document.getElementsByClassName(".c-name").value =
+    selectedRow.cells[0].innerHTML;
+  document.getElementsByClassName(".input-street").value =
+    selectedRow.cells[1].innerHTML;
+  document.getElementsByClassName(".phone").value =
+    selectedRow.cells[2].innerHTML;
+  document.getElementsByClassName(".key").value =
+    selectedRow.cells[3].innerHTML;
+  document.getElementsByClassName(".quantity").value =
+    selectedRow.cells[4].innerHTML;
+  // document.getElementsByClassName(".stop-notes").value =
+  //   selectedRow.cells[5].innerHTML;
+}
 
-  let cel1 = newRow.insertCell(0);
-  let cel2 = newRow.insertCell(1);
-  let cel3 = newRow.insertCell(2);
-  let cel4 = newRow.insertCell(3);
-  let cel5 = newRow.insertCell(4);
-  let cel6 = newRow.insertCell(5);
-
-  cel1.innerHTML = list1[x];
-  cel2.innerHTML = list2[x];
-  cel3.innerHTML = list3[x];
-  cel4.innerHTML = list4[x];
-  cel5.innerHTML = list5[x];
-  cel6.innerHTML = list6[x];
-
-  n++;
-  x++;
+function updateRecord(formData) {
+  selectedRow.cells[0].innerHTML = formData.c - name;
+  selectedRow.cells[1].innerHTML = formData.input - street;
+  selectedRow.cells[2].innerHTML = formData.phone;
+  selectedRow.cells[3].innerHTML = formData.key;
+  selectedRow.cells[4].innerHTML = formData.quantity;
 }
 
 // Modal open/close
